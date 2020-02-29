3aab55957c11508c142b854b7636ff2f785f901b
Jenny Håkansson <jennyomatic@gmail.com>
2020-02-29 17:05:03 +0100

    Set up database connection

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,23 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const { Client } = require('pg')
+
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'todo_db',
+    user: 'postgres',
+    password: 'postgres',
+})
+
+client.connect(err => {
+    if (err) {
+        console.error('Failed to connect to database:', err.stack)
+    } else {
+        console.log('Successfully connected to database!')
+    }
+})
+
+const app = express()
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Nothing here yet.'))
