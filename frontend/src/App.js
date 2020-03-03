1546307bace761baaf15170d52fac037af42a63e
Jenny Håkansson <jennyomatic@gmail.com>
2020-03-03 18:01:33 +0100

    Added first version of menu bar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
 import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
+import MenuBar from "./components/MenuBar";
 
 const styles = {
   container: {
     display: 'flex',
     flexDirection: 'row',
     margin: 50,
+  },
+  listContainer: {
+    display: 'flex',
+    flexDirection: 'column',
   },
   addTodo: {
     marginLeft: 200,
@@ -18,7 +23,10 @@ const styles = {
 function App() {
   return (
     <div style={styles.container}>
+      <div style={styles.listContainer}>
+      <MenuBar />
       <TodoList />
+      </div>
       <AddTodo style={styles.addTodo} />
     </div>
   );
